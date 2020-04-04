72fcedb4262c3fccfe623fb6604dcf25fe9d772d
hua-hnust <2359923694@qq.com>
2020-04-04 17:49:47 +0800

    跨域请求

--- src/main/java/com/farm/configurations/WebConfig.java
+++ src/main/java/com/farm/configurations/WebConfig.java
@@ -8,6 +8,7 @@ import com.farm.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -49,6 +50,19 @@ public class WebConfig implements WebMvcConfigurer {
 
         registry.addInterceptor(userInterceptor).addPathPatterns("/user/**");
         // TODO 各个角色拦截器
+    }
+
+    /**
+     *  跨域请求
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .maxAge(3600)
+                .allowCredentials(true);
     }
 
 }
